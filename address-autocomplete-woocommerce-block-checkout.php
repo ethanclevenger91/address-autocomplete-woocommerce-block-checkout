a82d406d5217b933fa50e7156430da400e010a1c
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2025-01-23 11:58:53 -0800

    Double header.

--- address-autocomplete-woocommerce-block-checkout.php
+++ address-autocomplete-woocommerce-block-checkout.php
@@ -3,11 +3,10 @@
 * Plugin Name:       Address Autocomplete for Woocommerce Block Checkout
 * Description:       Example block scaffolded with Create Block tool.
 * Author:            Sterner Stuff, eclev91
-* Author URI:	 	  https://sternerstuff.dev
+* Author URI:	 	 https://sternerstuff.dev
 * Requires at least: 6.1
 * Requires PHP:      7.0
 * Version:           0.1.0
-* Author:            The WordPress Contributors
 * License:           GPL-2.0-or-later
 * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 * Text Domain:       address-autocomplete-woocommerce-block-checkout
